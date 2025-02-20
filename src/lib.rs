78e895cd70cd07cc8802a369cd0ccd8d8017af16
Mithronn <denizz_kiziroglu@hotmail.com>
2025-02-20 15:13:47 +0300

    20.02.2025 * Dummy SVG fill implementation

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,17 @@ pub mod structs;
 pub mod utils;
 
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
     io::{BufReader, Cursor},
     path::Path,
 };
 
-use image::{GrayImage, ImageReader};
 use imageproc::{
-    contours::{find_contours_with_threshold, Contour},
-    edges::canny,
+    contours::find_contours_with_threshold,
+    image::{
+        imageops::{resize, FilterType},
+        GrayImage, ImageBuffer, ImageReader, Luma, Rgba,
+    },
 };
 use log::{info, trace, warn};
 use svg::{
@@ -71,67 +73,52 @@ where
 pub fn create_svg(image_byte: &[u8]) -> String {
     trace!("SVG Creation");
 
-    let image_reader = ImageReader::new(BufReader::new(Cursor::new(image_byte)))
+    // ------- Load the image -------
+    let mut image_reader = ImageReader::new(BufReader::new(Cursor::new(image_byte)))
         .with_guessed_format()
         .unwrap()
         .decode()
-        .unwrap();
+        .unwrap()
+        .to_rgba8();
 
-    info!(
-        "Image readed {}x{}",
-        image_reader.width(),
-        image_reader.height()
-    );
+    let (mut width, mut height) = image_reader.dimensions();
+    info!("Image readed {}x{}", width, height);
 
-    let mut gray_image = image_reader.to_luma8();
+    // ------- Upscale the image if necessary -------
+    if width * height < 512 * 512 {
+        let scale_factor = 3;
+        width = width * scale_factor;
+        height = height * scale_factor;
 
-    if gray_image.width() * gray_image.height() < 512 * 512 {
-        gray_image = upscale_image(&gray_image, 3);
-        warn!(
-            "Image size is small. Upscalled to {}x{}",
-            gray_image.width(),
-            gray_image.height()
-        );
+        image_reader = resize(&image_reader, width, height, FilterType::CatmullRom);
+
+        warn!("Image size is small. Upscalled to {}x{}", width, height);
     }
 
-    let low_threshold = 10.0 as f32;
-    let high_threshold = 60.0 as f32;
+    // --- Quantize the Image Colors ---
+    // Here we reduce color variety by rounding each channel to the nearest multiple.
+    let quantization_factor: u8 = 128;
+    let mut quantized_image = ImageBuffer::new(width, height);
+    let mut unique_colors = HashSet::new();
 
-    // TODO: FEATURE maybe ?
-    // let otsu_thresh = otsu_level(&gray_image);
-    // let low_threshold = otsu_thresh as f32 * 0.2;
-    // let high_threshold = otsu_thresh as f32;
+    for (x, y, pixel) in image_reader.enumerate_pixels() {
+        let r = (pixel[0] / quantization_factor) * quantization_factor;
+        let g = (pixel[1] / quantization_factor) * quantization_factor;
+        let b = (pixel[2] / quantization_factor) * quantization_factor;
+        // let r = pixel[0];
+        // let g = pixel[1];
+        // let b = pixel[2];
+        let quant_pixel = Rgba([r, g, b, pixel[3]]);
+        quantized_image.put_pixel(x, y, quant_pixel);
+        unique_colors.insert((r, g, b));
+    }
 
-    gray_image = canny(&gray_image, low_threshold, high_threshold);
-    // gray_image = close_gaps(&gray_image);
-
+    // ------- SVG container created -------
     let mut document = Document::new()
-        .set("width", gray_image.width())
-        .set("height", gray_image.height())
-        .set("viewBox", (0, 0, gray_image.width(), gray_image.height()));
+        .set("width", width)
+        .set("height", height)
+        .set("viewBox", (0, 0, width, height));
 
-    gray_image.save("assets/binary_image.png").unwrap();
-
-    let contours = find_contours_with_threshold::<u32>(&gray_image, 128);
-
-    // Set an error tolerance (in pixels)
-    let tolerance = 0.4;
-    contour_to_svg(&mut document, &contours, tolerance as f64);
-
-    info!(
-        "SVG created! Byte: {}",
-        document.to_string().as_bytes().len()
-    );
-
-    document.to_string()
-}
-
-fn upscale_image(img: &GrayImage, scale_factor: u32) -> GrayImage {
-    let (width, height) = (img.width() * scale_factor, img.height() * scale_factor);
-    image::imageops::resize(img, width, height, image::imageops::FilterType::CatmullRom)
-}
-
-pub fn contour_to_svg(document: &mut Document, contours: &[Contour<u32>], tolerance: f64) {
     let mut defs = Definitions::new();
     let mut stroke_group = Group::new().set("stroke-width", "1px");
     let mut fill_group = Group::new();
@@ -139,73 +126,91 @@ pub fn contour_to_svg(document: &mut Document, contours: &[Contour<u32>], tolera
     let mut strokes: HashMap<String, Vec<String>> = HashMap::new();
     let mut fills: HashMap<String, Vec<String>> = HashMap::new();
 
-    for (i, contour) in contours.iter().enumerate() {
-        let simplified = contour
-            .points
-            .iter()
-            .map(|&p| Point::new(p.x as f64, p.y as f64))
-            .collect::<Vec<Point>>();
-
-        let simplify_tolerance = 2 as f64;
-        let simplified = rdp(&simplified, simplify_tolerance);
-        let segments = fit_curve(&simplified, tolerance);
-
-        // Build SVG path data
-        let mut data = Data::new();
-
-        if let Some(first) = segments.first() {
-            let move_to = match first {
-                Segment::Line { start, .. } => (trunc(start.x), trunc(start.y)),
-                Segment::Cubic(curve) => (trunc(curve.p0.x), trunc(curve.p0.y)),
-            };
-            data.append(Command::Move(
-                Position::Absolute,
-                vec![move_to.0, move_to.1].into(),
-            ));
-        }
-
-        for segment in &segments {
-            match segment {
-                Segment::Line { end, .. } => {
-                    data.append(Command::Line(
-                        Position::Absolute,
-                        vec![trunc(end.x), trunc(end.y)].into(),
-                    ));
-                }
-                Segment::Cubic(curve) => {
-                    data.append(Command::CubicCurve(
-                        Position::Absolute,
-                        vec![
-                            trunc(curve.p1.x),
-                            trunc(curve.p1.y),
-                            trunc(curve.p2.x),
-                            trunc(curve.p2.y),
-                            trunc(curve.p3.x),
-                            trunc(curve.p3.y),
-                        ]
-                        .into(),
-                    ));
-                }
+    // ------- Process each unique colors -------
+    for color in unique_colors {
+        // Build a binary mask for the current color
+        let mut mask: GrayImage = GrayImage::new(width, height);
+        for (x, y, pixel) in quantized_image.enumerate_pixels() {
+            let (r, g, b, a) = (pixel[0], pixel[1], pixel[2], pixel[3]);
+            if (r, g, b) == color && a > 0 {
+                mask.put_pixel(x, y, Luma([255]));
+            } else {
+                mask.put_pixel(x, y, Luma([0]));
             }
         }
 
-        if !data.is_empty() {
-            data.append(Command::Close);
+        // Extract contours from the mask
+        let contours = find_contours_with_threshold::<u32>(&mask, 1);
 
-            let id = generate_id(i);
+        for (i, contour) in contours.iter().enumerate() {
+            // Convert the color to a hex string for SVG.
+            let fill_color = format!("#{:02X}{:02X}{:02X}", color.0, color.1, color.2);
 
-            let path = SVGPath::new().set("id", id.clone()).set("d", data);
-            defs.append(path);
+            let simplified = contour
+                .points
+                .iter()
+                .map(|&p| Point::new(p.x as f64, p.y as f64))
+                .collect::<Vec<Point>>();
 
-            strokes
-                .entry("black".to_string())
-                .or_insert_with(Vec::new)
-                .push(id.clone());
+            let tolerance = 1 as f64;
+            let simplify_tolerance = 2 as f64;
+            let simplified = rdp(&simplified, simplify_tolerance);
+            let segments = fit_curve(&simplified, tolerance);
 
-            fills
-                .entry("none".to_string())
-                .or_insert_with(Vec::new)
-                .push(id);
+            // Build SVG path data
+            let mut data = Data::new();
+
+            if let Some(first) = segments.first() {
+                let move_to = match first {
+                    Segment::Line { start, .. } => (trunc(start.x), trunc(start.y)),
+                    Segment::Cubic(curve) => (trunc(curve.p0.x), trunc(curve.p0.y)),
+                };
+                data.append(Command::Move(
+                    Position::Absolute,
+                    vec![move_to.0, move_to.1].into(),
+                ));
+            }
+
+            for segment in &segments {
+                match segment {
+                    Segment::Line { end, .. } => {
+                        data.append(Command::Line(
+                            Position::Absolute,
+                            vec![trunc(end.x), trunc(end.y)].into(),
+                        ));
+                    }
+                    Segment::Cubic(curve) => {
+                        data.append(Command::CubicCurve(
+                            Position::Absolute,
+                            vec![
+                                trunc(curve.p1.x),
+                                trunc(curve.p1.y),
+                                trunc(curve.p2.x),
+                                trunc(curve.p2.y),
+                                trunc(curve.p3.x),
+                                trunc(curve.p3.y),
+                            ]
+                            .into(),
+                        ));
+                    }
+                }
+            }
+
+            if !data.is_empty() {
+                data.append(Command::Close);
+
+                let id = generate_id(i);
+
+                let path = SVGPath::new().set("id", id.clone()).set("d", data);
+                defs.append(path);
+
+                strokes
+                    .entry(fill_color.clone())
+                    .or_insert_with(Vec::new)
+                    .push(id.clone());
+
+                fills.entry(fill_color).or_insert_with(Vec::new).push(id);
+            }
         }
     }
 
@@ -234,6 +239,13 @@ pub fn contour_to_svg(document: &mut Document, contours: &[Contour<u32>], tolera
     document.append(defs);
     document.append(stroke_group);
     document.append(fill_group);
+
+    info!(
+        "SVG created! Byte: {}",
+        document.to_string().as_bytes().len()
+    );
+
+    document.to_string()
 }
 
 #[cfg(test)]
